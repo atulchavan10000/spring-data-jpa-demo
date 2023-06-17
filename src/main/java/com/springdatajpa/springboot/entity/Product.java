1e13a748078344bb67e7424fb2669938697a4685
Atul <atulchavan528@gmail.com>
2023-06-17 21:32:59 +0530

    customizing Product entity with JPA annotations

--- src/main/java/com/springdatajpa/springboot/entity/Product.java
+++ src/main/java/com/springdatajpa/springboot/entity/Product.java
@@ -1,20 +1,42 @@
 package com.springdatajpa.springboot.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.Columns;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(
+        name = "products",
+        schema = "ecommerce",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "sku_unique",
+                        columnNames = "stock_keeping_unit"
+                )
+        }
+)
 public class Product {
 
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_generator"
+    )
+    @SequenceGenerator(
+            name = "product_generator",
+            sequenceName = "product_sequence_name",
+            allocationSize = 1
+    )
     private Long id;
+    @Column(name = "stock_keeping_unit", nullable = false)
     private String sku;
+    @Column(nullable = false)
     private String name;
+    @Column(length = 500)// default length is 255
     private String description;
     private BigDecimal price;
     private boolean active;
